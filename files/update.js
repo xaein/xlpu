db6bece6047e150b7d33270140f578f00b9969c4
xaein <xaein001@gmail.com>
2024-09-16 17:21:49 +1000

    Update update.js

--- files/update.js
+++ files/update.js
@@ -52,7 +52,7 @@ function scrollToLine(element, text) {
     const lines = element.value.split('\n');
     const lineIndex = lines.findIndex(line => line.includes(text));
     if (lineIndex !== -1) {
-        const lineHeight = 10; // Approximate line height in pixels
+        const lineHeight = 9; // Approximate line height in pixels
         element.scrollTop = lineIndex * lineHeight;
     }
 }
